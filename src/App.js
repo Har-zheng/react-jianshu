fa07b97051780c7ddc990159ed7488f4f3aa9fa7
unknown <1456421562@qq.com>
2019-09-22 22:40:30 +0800

    feat: icon 动画的实现

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { GlobalStyle } from './style'
 import Header from './common/header'
 import store from './store/index'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       // react-redux 提供数据  供子选项里的所有组件可以访问数据
       <Provider store={store}>
-        <GlobalStyle/>
-        <Header></Header>
+        <div>
+          <GlobalStyle />
+          <Header></Header>
+          <BrowserRouter>
+            <div>
+              <Route path='/' exact render={() => <div>home</div>}></Route>
+              <Route path='/detail' exact render={() => <div>detail</div>}></Route>
+            </div>
+          </BrowserRouter>
+        </div>
       </Provider>
     )
   }
